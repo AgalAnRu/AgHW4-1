c8e507f471dd887e033274b239014c60eb62afc1
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2022-09-15 19:57:01 +0300

    Добавьте файлы проекта.

---
+++ AgMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AgHW4_1
+{
+    internal class AgMenu
+    {
+        string[] menuItems;
+        int counter;
+        internal AgMenu(string[] menuItems) => this.menuItems = menuItems;
+        internal int GetSelectedMenuItem()
+        {
+            ConsoleKey key;
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    if (i == counter)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    Console.WriteLine(menuItems[i]);
+                    if (i == counter)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+                Console.WriteLine("(Выберите пункт меню и нажмите Enter " +
+                    "или нажмите Escape для выхода)");
+                key = Console.ReadKey().Key;
+                if (key == ConsoleKey.DownArrow)
+                {
+                    counter++;
+                    if (counter == menuItems.Length)
+                        counter = 0;
+                }
+                if (key == ConsoleKey.UpArrow)
+                {
+                    counter--;
+                    if (counter == -1)
+                        counter = menuItems.Length - 1;
+                }
+            } while (key != ConsoleKey.Enter && key != ConsoleKey.Escape);
+            if (key == ConsoleKey.Escape)
+                counter = menuItems.Length - 1;
+            return counter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace AgHW4_1
+{
+    internal class Program
+    {
+        static string promtPressAnyKey = "Нажмите любую клавишу для продолжения...";
+        //private static GetInput getInput = new GetInput();
+        private static Random rnd = new Random();
+        delegate void method();
+        static string[] menuMainItems = new string[5];
+        static string[] menuFoursItems = new string[7];
+        static AgMenu menuMain = new AgMenu(menuMainItems);
+        static AgMenu menuFours = new AgMenu(menuFoursItems);
+        static void Main(string[] args)
+        {
+            MenuInitialization();
+            //method[] methods = new method[] { Task1Part1, Task1Part2, Task1Part3, Task1Part4,
+            //    Task2, Task3, Task4, QuitFromConsole};
+            int selectedMenuItem;
+            //AgMenu menuMain = new AgMenu(menuMainItems);
+            
+            ShowMainScreen();
+            
+        }
+        static void MenuInitialization()
+        {
+            menuMainItems[0] = "Урок 1";
+            menuMainItems[1] = "Урок 2";
+            menuMainItems[2] = "Урок 3";
+            menuMainItems[3] = "Урок 4";
+            menuMainItems[4] = "Выход";
+
+            menuFoursItems[0] = "Задание 1";
+            menuFoursItems[1] = "Задание 2";
+            menuFoursItems[2] = "Задание 3";
+            menuFoursItems[3] = "Задание 4";
+            menuFoursItems[4] = "Задание 5";
+            menuFoursItems[5] = "Задание 6";
+            menuFoursItems[6] = "<<Возврат";
+        }
+        static void ShowMainScreen()
+        {
+            int selectedMenuItem = 0;
+            selectedMenuItem = menuMain.GetSelectedMenuItem();
+            switch (selectedMenuItem)
+            {
+                case 0:
+                    ShowFirstScreen();
+                    break;
+                case 1:
+                    ShowSecondScreen();
+                    break;
+                case 2:
+                    ShowThirdScreen();
+                    break;
+                case 3:
+                    ShowFoursScreen();
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        static void ShowFirstScreen()
+        {
+            Console.WriteLine("Раздел в разработке (выберите другой пункт)");
+            Console.ReadKey();
+            ShowMainScreen();
+        }
+        static void ShowSecondScreen()
+        {
+            Console.WriteLine("Раздел в разработке (выберите другой пункт)");
+            Console.ReadKey();
+            ShowMainScreen();
+        }
+        static void ShowThirdScreen()
+        {
+            Console.WriteLine("Раздел в разработке (выберите другой пункт)");
+            Console.ReadKey();
+            ShowMainScreen();
+        }
+        static void ShowFoursScreen()
+        {
+            int selectedMenuItem = 0;
+            do
+            {
+                selectedMenuItem = menuFours.GetSelectedMenuItem();
+                Console.Clear();
+                //methods[selectedMenuItem]();
+                if (selectedMenuItem == menuFoursItems.Length - 1)
+                    continue;
+                NextScreen();
+            } while (selectedMenuItem != menuFoursItems.Length - 1);
+            ShowMainScreen();
+        }
+        static void QuitFromConsole()
+        {
+            //NextScreen();
+        }
+        static void NextScreen()
+        {
+            Console.WriteLine(promtPressAnyKey);
+            Console.ReadKey();
+        }
+    }
+}
